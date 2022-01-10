9e39413786f6e529c2305af5fda04a5e1899a239
alexa-woodward <42977807+alexa-woodward@users.noreply.github.com>
2022-01-10 15:47:36 -0500

    Update survival_OfflineEnvironment.py

--- skExSTraCS/survival_OfflineEnvironment.py
+++ skExSTraCS/survival_OfflineEnvironment.py
@@ -1,4 +1,4 @@
-from skExSTraCS.DataManagement import survival_DataManagement
+from skExSTraCS.DataManagement import DataManagement
 
 class OfflineEnvironment:
     def __init__(self,dataFeatures,dataEventTimes,dataEventStatus,model):
